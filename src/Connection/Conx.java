48f4d05329a1caa121baf3ce0acdd6f228d76c25
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-06-18 14:29:55 -0600

    Credenciales cambios

---
+++ src/Connection/Conx.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author LEAC2
+ */
+public class Conx {
+    public static Connection Conectar(){
+        String cadena = "jdbc:sqlserver://localhost:1433;"
+            + "databaseName=VetSoft;"
+            + "user=userSQL;"
+            + "password=pasf2;"
+            + "encrypt=false;"
+            + "loginTimeout=10;";
+        
+        try {
+            Connection con = DriverManager.getConnection(cadena);
+            return con;
+        } catch(SQLException ex) {
+            System.out.println(ex.toString());
+            return null;
+        }
+    }
+}
