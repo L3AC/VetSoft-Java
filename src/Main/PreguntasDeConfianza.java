11963e19f6bba00684189c369debae17a71dcce7
Gerson <20220175@ricaldone.edu.sv>
2023-06-26 11:06:50 -0600

    Usuario

--- src/Main/PreguntasDeConfianza.java
+++ src/Main/PreguntasDeConfianza.java
@@ -13,6 +13,7 @@ public class PreguntasDeConfianza extends javax.swing.JFrame {
     /**
      * Creates new form PreguntasDeConfianza
      */
+    String cambio;
     public PreguntasDeConfianza() {
         initComponents();
     }
