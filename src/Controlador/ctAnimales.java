9a5a65d34aa1272d1ec350b0a7b38722e93470ed
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-06-27 08:57:51 -0600

    Credenciales cambios

---
+++ src/Controlador/ctAnimales.java
@@ -0,0 +1,17 @@
+
+package Controlador;
+
+public class ctAnimales {
+    public int idAnimal;
+
+    
+    
+    public int getIdAnimal() {
+        return idAnimal;
+    }
+    public void setIdAnimal(int idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+    
+    
+}
