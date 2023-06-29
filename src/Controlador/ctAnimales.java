339203844dd104e647db6acdc46e4e6d0a0e758e
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-06-28 20:12:34 -0600

    Credenciales cambios

--- src/Controlador/ctAnimales.java
+++ src/Controlador/ctAnimales.java
@@ -1,70 +1,99 @@
-
 package Controlador;
+
+import Modelo.mdAnimales;
 import java.sql.*;
 
 public class ctAnimales {
+
     public int idAnimal;
     public int idCliente;
     public int idRaza;
     public String padecimientos;
     public String nombre;
-    public int peso;
+    public String peso;
     public String edad;
     public String sexo;
 
     public int getIdAnimal() {
         return idAnimal;
     }
+
     public void setIdAnimal(int idAnimal) {
         this.idAnimal = idAnimal;
     }
+
     public int getIdCliente() {
         return idCliente;
     }
+
     public void setIdCliente(int idCliente) {
         this.idCliente = idCliente;
     }
+
     public int getIdRaza() {
         return idRaza;
     }
+
     public void setIdRaza(int idRaza) {
         this.idRaza = idRaza;
     }
+
     public String getPadecimientos() {
         return padecimientos;
     }
+
     public void setPadecimientos(String padecimientos) {
         this.padecimientos = padecimientos;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    public int getPeso() {
+
+    public String getPeso() {
         return peso;
     }
-    public void setPeso(int peso) {
+
+    public void setPeso(String peso) {
         this.peso = peso;
     }
+
     public String getEdad() {
         return edad;
     }
+
     public void setEdad(String edad) {
         this.edad = edad;
     }
+
     public String getSexo() {
         return sexo;
     }
+
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
-   
-   public ResultSet  cargarData(){
-       
-      return null;
-   }
-    
-    
+
+    mdAnimales ani = new mdAnimales();
+
+    public ResultSet cargarAnim() {
+        return ani.cargarAnim();
+    }
+
+    public boolean insertAnim() {
+        return ani.insertAnim(idCliente, idRaza, padecimientos, nombre, peso, edad, sexo);
+    }
+
+    public boolean updtAnim() {
+        return ani.updateAnim(idAnimal, idRaza, padecimientos, nombre, peso, edad, sexo);
+    }
+
+    public boolean deleteAnim() {
+        return ani.deleteAnim();
+    }
+
 }
