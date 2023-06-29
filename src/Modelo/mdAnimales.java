c5020a51bdd87a7a57c967b865c81cc00c3fa65c
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-06-28 18:10:39 -0600

    Credenciales cambios

--- src/Modelo/mdAnimales.java
+++ src/Modelo/mdAnimales.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
-/**
- *
- * @author LEAC2
- */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 public class mdAnimales {
+
+    Connection con = Conx.Conectar();
+    ResultSet rs;
+    PreparedStatement ps;
+
+    private ResultSet SelectAnim() {
+        String query = "select * from tbAnimales";
+        try {
+            ps = con.prepareStatement(query);
+            rs = ps.executeQuery();
+            
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejo de la excepción SQLException
+            System.out.println(e.toString());
+            JOptionPane.showMessageDialog(null, "Error al cargar");
+            return null; //DIO ERROR
+        }
+    }
     
+        private boolean updateAnim(int idA,int idR,String padec,String nombre,
+                String peso,String edad,String sexo ) {
+        String query = "EXEC updtAnim ?,?,?,?,?,?,?;";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setInt(1, idA);
+            ps.setInt(2, idR);
+            ps.setString(3, padec);
+            ps.setString(4, nombre);
+            ps.setString(5, peso);
+            ps.setString(6, edad);
+            ps.setString(7, sexo);
+            ps.executeQuery();
+             JOptionPane.showMessageDialog(null, "Campos actualizados");
+            return true;
+            
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejo de la excepción SQLException
+            System.out.println(e.toString());
+             JOptionPane.showMessageDialog(null, "Error al ejecutar");
+            return false; //DIO ERROR
+        }
+    }
 }
