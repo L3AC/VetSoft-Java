e688025f0c232e4b4ea6ed8b7fad56db1df38e83
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2023-06-29 22:30:31 -0600

    Credenciales cambios

--- src/AControlador/ctCitas.java
+++ src/AControlador/ctCitas.java
@@ -1,5 +1,101 @@
 package AControlador;
+import AModelo.mdCitas;
 import java.sql.*;
 public class ctCitas {
+    public int idCita;
+    public int idAnimal ;
+    public int idTipoServicio;
+    public int idDoctor;
+    public String estado;
+    public String notaDelCliente ;
+    public String notaDelDoctor ;
+    public String fechahora;
+
+    public int getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(int idCita) {
+        this.idCita = idCita;
+    }
+
+    public int getIdAnimal() {
+        return idAnimal;
+    }
+
+    public void setIdAnimal(int idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public int getIdTipoServicio() {
+        return idTipoServicio;
+    }
+
+    public void setIdTipoServicio(int idTipoServicio) {
+        this.idTipoServicio = idTipoServicio;
+    }
+
+    public int getIdDoctor() {
+        return idDoctor;
+    }
+
+    public void setIdDoctor(int idDoctor) {
+        this.idDoctor = idDoctor;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNotaDelCliente() {
+        return notaDelCliente;
+    }
+
+    public void setNotaDelCliente(String notaDelCliente) {
+        this.notaDelCliente = notaDelCliente;
+    }
+
+    public String getNotaDelDoctor() {
+        return notaDelDoctor;
+    }
+
+    public void setNotaDelDoctor(String notaDelDoctor) {
+        this.notaDelDoctor = notaDelDoctor;
+    }
+
+    public String getFechahora() {
+        return fechahora;
+    }
+
+    public void setFechahora(String fechahora) {
+        this.fechahora = fechahora;
+    }
     
+     mdCitas cl = new mdCitas();
+
+    public ResultSet cargarCita() {
+        return cl.cargarCita();
+    }
+        public ResultSet selectCita() {
+        return cl.selectCita(idCita);
+    }
+
+    public boolean insertCita() {
+        return true;
+       // return cl.insertCita(idCliente,nombre, apellido, dui, nacimiento, sexo);
+    }
+
+    public boolean updtCita() {
+        return true;
+        //return cl.updateCita(idCliente,nombre, apellido, dui, nacimiento, sexo);
+    }
+
+    public boolean deleteCita() {
+        return true;
+       // return cl.deleteCl(idCliente);
+    }
 }
--- src/AModelo/mdCitas.java
+++ src/AModelo/mdCitas.java
@@ -1,6 +1,101 @@
 
 package AModelo;
-
+import java.sql.*;
+import javax.swing.JOptionPane;
 public class mdCitas {
-    
+    Connection con = Conx.Conectar();
+    ResultSet rs;
+    PreparedStatement ps;
+
+    public ResultSet cargarCita() {
+        String query = "select idCliente,CONCAT(nombre,' ',apellido) as 'Nombre',DATEDIFF(YEAR, nacimiento, GETDATE()) as 'Edad'\n"
+                + ",sexo from tbClientes;";
+        try {
+            ps = con.prepareStatement(query);
+            rs = ps.executeQuery();
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejo de la excepción SQLException
+            System.out.println(e.toString());
+            JOptionPane.showMessageDialog(null, "Error al ejecutar");
+            return null; //DIO ERROR
+        }
+    }
+        public ResultSet selectCita(int idC) {
+        String query = "select * from tbCitas where idCita=?;";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setInt(1, idC);
+            rs = ps.executeQuery();
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejo de la excepción SQLException
+            System.out.println(e.toString());
+            JOptionPane.showMessageDialog(null, "Error al ejecutar");
+            return null; //DIO ERROR
+        }
+    }
+
+    public boolean deleteCita(int idC) {
+        String query = "DELETE tbCitas where idCita=?;";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setInt(1, idC);
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Registro eliminado");
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejo de la excepción SQLException
+            System.out.println(e.toString());
+            JOptionPane.showMessageDialog(null, "Error al ejecutar");
+            return false;
+        }
+    }
+
+    public boolean insertCita(int idAni,int idTS,int idD, String notaCl, String notaDoc, String fechahora) {
+        String query = "insert into tbCitas values(?,?,?,'Pendiente',?,?,?,GETDATE())";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setInt(1, idAni);
+            ps.setInt(2, idTS);
+            ps.setInt(3, idD);
+            ps.setString(4, notaCl);
+            ps.setString(5, notaDoc);
+            ps.setString(6, fechahora);
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Campos ingresados");
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejo de la excepción SQLException
+            System.out.println(e.toString());
+            JOptionPane.showMessageDialog(null, "Error al ejecutar");
+            return false; //DIO ERROR
+        }
+    }
+
+    public boolean updateCita(int idCita,int idAni,int idTS,int idD, String estado, String nCl, String nDoc, String fechahora) {
+        String query = "update tbCitas set idAnimal=?,idTipoServicio=?,idDoctor=?,estado=?,"
+                + "notaDelCliente=?,notaDelDoctor=?,fechahora=? where idCita=?;";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setInt(1, idAni);
+            ps.setInt(2, idTS);
+            ps.setInt(3, idD);
+            ps.setString(4, estado);
+            ps.setString(5, nCl);
+            ps.setString(6, nDoc);
+            ps.setString(7,fechahora);
+            ps.setInt(8, idCita);
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Campos actualizados");
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejo de la excepción SQLException
+            System.out.println(e.toString());
+            JOptionPane.showMessageDialog(null, "Error al ejecutar");
+            return false; //DIO ERROR
+        }
+    }
 }
