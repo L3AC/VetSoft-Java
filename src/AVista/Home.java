ccd529b18be1fae290f808c78fee92fdf7ded980
Ariana <katherin.ariana.melgar@gmail.com>
2023-08-14 23:48:54 -0600

    Cambios

--- src/AVista/Home.java
+++ src/AVista/Home.java
@@ -10,6 +10,48 @@ package AVista;
  */
 public class Home extends javax.swing.JPanel {
 
+    /*public static void main(String[] args) throws InterruptedException{
+        
+        int horas=0, minutos=0, segundos=0;
+        
+        while(true){
+            //Mostrar
+            if(horas<10){
+                System.out.print("0");
+            }
+            System.out.println(horas+":");
+            
+            if(minutos<10){
+                System.out.print("0");
+            }
+            System.out.println(minutos+":");
+            
+            if(segundos<10){
+                System.out.print("0");
+            }
+            System.out.println(segundos);
+            
+            //Aumentar el tiempo
+            segundos++;
+            
+            //Comprobar el tiempo
+            if(segundos==60){
+                segundos=0;
+                minutos++;
+                if(minutos==60){
+                    minutos=0;
+                    horas++;
+                if(horas==24){
+                    horas=0;
+                }
+               }
+            }            
+            Thread.sleep(1000);        
+        }
+             
+    }*/
+    
+    
     /**
      * Creates new form Home
      */
